f50259a9950da4e50c13bdd8ddcc9486d1cc4983
Alex Lemenze <56348296+alemenze@users.noreply.github.com>
2020-09-08 14:18:37 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -22,7 +22,7 @@ class SurfStatus(MycroftSkill):
         # For initial tests this will only cover one location.
         # Eventually need to expand to be flexible. 
         report=report_pull('Belmar','5842041f4e65fad6a7708a01','2147','NJ/07715')
-        output=report[['data']['conditions'][0]['observation']]
+        output=report['data']['conditions'][0]['observation']
         self.speak(str(output))
         self.speak_dialog('status.surf')
 
